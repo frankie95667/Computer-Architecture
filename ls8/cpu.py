6ae21aa98a92cbc0d95b4d9b3c1ea5f3889d53a4
Anthony Kawa <anthonyk2020@gmail.com>
2020-08-18 14:44:39 -0700

    added Push, Pop, and ALU operations

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,9 +2,45 @@
 
 import sys
 
+NOP = 0b00000000
+
+LD  = 0b10000011
 LDI = 0b10000010
 PRN = 0b01000111
 HLT = 0b00000001
+
+AND = 0b10101000
+NOT = 0b01101001
+OR  = 0b10101010
+XOR = 0b10101011
+
+POP = 0b01000110
+PUSH= 0b01000101
+PRA = 0b01001000
+CALL= 0b01010000
+CMP = 0b10100111
+DEC = 0b01100110
+INC = 0b01100101
+INT = 0b01010010
+IRET= 0b00010011
+JEQ = 0b01010101
+JGE = 0b01011010
+JGT = 0b01010111
+JLE = 0b01011001 
+JLT = 0b01011000
+JMP = 0b01010100
+JNE = 0b01010110
+RET = 0b00010001
+SHL = 0b10101100
+SHR = 0b10101101
+ST  = 0b10000100
+
+# ALU
+ADD = 0b10100000
+SUB = 0b10100001
+MUL = 0b10100010
+DIV = 0b10100011
+MOD = 0b10100100 
 
 class CPU:
     """Main CPU class."""
@@ -12,36 +48,32 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.pc = 0
+        self.ie = 0
+        self.fl = 0
         self.ram = [0] * 256
         self.reg = [0] * 8
+        self.reg[7] = 0xF4
         self.running = False
     
     def ram_read(self, index):
         return self.ram[index]
 
-    def ram_write(self, index, value):
-        self.ram[index] = value
+    def ram_write(self, value, addr):
+        self.ram[addr] = value
 
-    def load(self):
+    def load(self, filename):
         """Load a program into memory."""
 
         address = 0
 
         # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
+        f = open(filename)
+        program = [line.split("#")[0] for line in f.read().split("\n") if line.split('#')[0] != '']
+        
         for instruction in program:
-            self.ram[address] = instruction
+            self.ram[address] = int(instruction, 2)
             address += 1
+        
 
 
     def alu(self, op, reg_a, reg_b):
@@ -49,7 +81,14 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "SUB":
+            self.reg[reg_a] -= self.reg[reg_b]
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "INC":
+            self.reg[reg_a] += 1
+        elif op == "DEC":
+            self.reg[reg_a] -= 1
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -77,13 +116,29 @@ class CPU:
         """Run the CPU."""
         self.running = True
         while self.running:
-            self.trace()
-            if self.ram_read(self.pc) == LDI:
-                self.reg[self.ram_read(self.pc + 1)] = self.ram_read(self.pc + 2)
-                self.pc += 3
-            elif self.ram_read(self.pc) == PRN:
-                print(self.reg[self.ram_read(self.pc + 1)])
-                self.pc += 2
-            elif self.ram_read(self.pc) == HLT:
+            # self.trace()
+            IR = self.ram_read(self.pc)
+            num_of_operands = (IR >> 6) + 1
+            operand_a, operand_b = self.ram_read(self.pc + 1), self.ram_read(self.pc + 2)
+
+            if IR == LDI:
+                self.reg[operand_a] = operand_b
+            elif IR == PRN:
+                print(self.reg[operand_a])
+            elif IR == MUL:
+                self.alu("MUL", operand_a, operand_b)
+            elif IR == PUSH:
+                self.alu("DEC", 7, None)
+                self.ram_write(self.reg[operand_a], self.reg[7])
+            elif IR == POP:
+                self.reg[operand_a] = self.ram_read(self.reg[7])
+                self.alu("INC", 7, None)
+            elif IR == CALL:
+                self.alu("DEC", 7, None)
+                self.ram_write(self.reg[operand_a], self.reg[7])
+                self.pc = self.reg[operand_a]
+                self
+            elif IR == HLT:
                 self.running = False
+            self.pc += num_of_operands
 
